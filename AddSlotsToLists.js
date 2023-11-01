0f8340da9fbf3e04ae853b07915a8bda227257c6
WB-Painel <126594701+WB-Painel@users.noreply.github.com>
2023-10-31 23:00:59 -0300

    Update AddSlotsToLists.js

--- AddSlotsToLists.js
+++ AddSlotsToLists.js
@@ -3,7 +3,9 @@ let LIST_1_BLOCOS_HEIGHT = 0;
 	  let LIST_2_BLOCOS_HEIGHT = 0;
 	  
 	  let LIST_3_BLOCOS_HEIGHT = 0;
-	  
+
+export { LIST_1_BLOCOS_HEIGHT };
+
 function C2(){
 	  
 	 let LISTA_1 = document.getElementsByClassName("LISTA_1")[0];
