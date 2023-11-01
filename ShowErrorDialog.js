7efd421f9866dd6c2ccd8ebe0551eb631e86007e
WB-Painel <126594701+WB-Painel@users.noreply.github.com>
2023-11-01 16:25:37 -0300

    Add files via upload

---
+++ ShowErrorDialog.js
@@ -0,0 +1,74 @@
+function ShowErrorDialog(ImageName){
+				
+				let ViewErrorDialog = document.getElementsByClassName("ViewErrorDialog")[0];
+				
+				let i = -ViewErrorDialog.clientWidth;
+				
+				ViewErrorDialog.style.display="block";
+				
+				//PC
+				if(window.screen.width>window.screen.height){
+				
+				let Interval = setInterval(function(){
+				
+				if(!0>i){
+				
+				i=i+10;
+				
+				ViewErrorDialog.style.left=i+"px";
+				
+				}else{
+				
+				ViewErrorDialog.style.left=0+"px";
+				
+				clearInterval(Interval);
+				
+				}
+				
+				},10);
+				
+				}
+				
+				ViewErrorDialog.style.display="block";
+				
+				ViewErrorDialog.onclick = function(){
+				
+				};
+				
+				let ErrorDialogMessage = document.getElementsByClassName("ErrorDialogMessage")[0];
+				ErrorDialogMessage.style.backgroundImage="url(image/"+ImageName+".png)";
+				
+				let ErrorDialogButtonRecarregar = document.getElementsByClassName("ErrorDialogButtonRecarregar")[0];
+				
+				ErrorDialogButtonRecarregar.onclick = function(){
+				
+				if(window.screen.width>window.screen.height){
+				
+				let Interval = setInterval(function(){
+				
+				if(i>-ViewErrorDialog.clientWidth){
+				
+				i=i-10;
+				
+				ViewErrorDialog.style.left=i+"px";
+				
+				}else{
+				
+				
+				ViewErrorDialog.style.left=-ViewErrorDialog.clientWidth+"px";
+				
+				clearInterval(Interval);
+				
+				}
+				
+				},10);
+				
+				}else{
+				
+				ViewErrorDialog.style.display="none";
+				
+				}
+				
+				};
+				
+				}
